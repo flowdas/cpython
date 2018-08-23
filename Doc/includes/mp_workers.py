335c2dad4e79909d2b546d7b8f49774f2906ee30
flowdas <flowdas@gmail.com>
2018-08-23 15:41:46 +0900

    annotate library/multiprocessing

--- Doc/includes/mp_workers.py
+++ Doc/includes/mp_workers.py
@@ -4,7 +4,7 @@ import random
 from multiprocessing import Process, Queue, current_process, freeze_support
 
 #
-# Function run by worker processes
+# 작업자 프로세스가 실행하는 함수
 #
 
 def worker(input, output):
@@ -13,7 +13,7 @@ def worker(input, output):
         output.put(result)
 
 #
-# Function used to calculate result
+# 결과를 계산하는데 사용되는 함수
 #
 
 def calculate(func, args):
@@ -22,7 +22,7 @@ def calculate(func, args):
         (current_process().name, func.__name__, args, result)
 
 #
-# Functions referenced by tasks
+# 작업이 참조하는 함수
 #
 
 def mul(a, b):
@@ -42,32 +42,32 @@ def test():
     TASKS1 = [(mul, (i, 7)) for i in range(20)]
     TASKS2 = [(plus, (i, 8)) for i in range(10)]
 
-    # Create queues
+    # 큐를 만듭니다
     task_queue = Queue()
     done_queue = Queue()
 
-    # Submit tasks
+    # 작업을 제출합니다
     for task in TASKS1:
         task_queue.put(task)
 
-    # Start worker processes
+    # 작업자 프로세스를 시작합니다
     for i in range(NUMBER_OF_PROCESSES):
         Process(target=worker, args=(task_queue, done_queue)).start()
 
-    # Get and print results
-    print('Unordered results:')
+    # 결과를 갖고와서 인쇄합니다
+    print('순서없는 결과:')
     for i in range(len(TASKS1)):
         print('\t', done_queue.get())
 
-    # Add more tasks using `put()`
+    # `put()` 을 사용해서 작업을 추가합니다
     for task in TASKS2:
         task_queue.put(task)
 
-    # Get and print some more results
+    # 결과를 더 갖고와서 인쇄합니다
     for i in range(len(TASKS2)):
         print('\t', done_queue.get())
 
-    # Tell child processes to stop
+    # 자식 프로세스에게 정지하라고 알립니다
     for i in range(NUMBER_OF_PROCESSES):
         task_queue.put('STOP')
 
