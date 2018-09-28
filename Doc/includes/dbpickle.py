7b1f365e805efd930aa3fec1f75560d034c40633
flowdas <flowdas@gmail.com>
2018-09-28 10:58:06 +0900

    annotate library/pickle

--- Doc/includes/dbpickle.py
+++ Doc/includes/dbpickle.py
@@ -1,25 +1,23 @@
-# Simple example presenting how persistent ID can be used to pickle
-# external objects by reference.
+# 지속성 ID를 외부 객체를 참조로 피클 하는데 사용하는 방법을 보여주는 간단한 예제.
 
 import pickle
 import sqlite3
 from collections import namedtuple
 
-# Simple class representing a record in our database.
+# 우리 데이터베이스의 레코드를 나타내는 간단한 클래스.
 MemoRecord = namedtuple("MemoRecord", "key, task")
 
 class DBPickler(pickle.Pickler):
 
     def persistent_id(self, obj):
-        # Instead of pickling MemoRecord as a regular class instance, we emit a
-        # persistent ID.
+        # MemoRecord를 일반 클래스 인스턴스로 피클 하는 대신, 지속성 ID를 출력합니다.
         if isinstance(obj, MemoRecord):
-            # Here, our persistent ID is simply a tuple, containing a tag and a
-            # key, which refers to a specific record in the database.
+            # 여기서, 우리의 지속성 ID는 태그와 데이터베이스의 특정 레코드를 참조하는 키를 포함하는
+            # 단순한 튜플입니다.
             return ("MemoRecord", obj.key)
         else:
-            # If obj does not have a persistent ID, return None. This means obj
-            # needs to be pickled as usual.
+            # obj에 지속성 ID가 없으면, None을 반환합니다. 이것은 obj가 평소와 같이 피클 되어야
+            # 함을 의미합니다.
             return None
 
 
@@ -30,19 +28,18 @@ class DBUnpickler(pickle.Unpickler):
         self.connection = connection
 
     def persistent_load(self, pid):
-        # This method is invoked whenever a persistent ID is encountered.
-        # Here, pid is the tuple returned by DBPickler.
+        # 이 메서드는, 지속성 ID를 만날 때마다 호출됩니다.
+        # 여기에서, pid는 DBPickler가 반환한 튜플입니다.
         cursor = self.connection.cursor()
         type_tag, key_id = pid
         if type_tag == "MemoRecord":
-            # Fetch the referenced record from the database and return it.
+            # 데이터베이스에서 참조 된 레코드를 반입하여 리턴하십시오.
             cursor.execute("SELECT * FROM memos WHERE key=?", (str(key_id),))
             key, task = cursor.fetchone()
             return MemoRecord(key, task)
         else:
-            # Always raises an error if you cannot return the correct object.
-            # Otherwise, the unpickler will think None is the object referenced
-            # by the persistent ID.
+            # 올바른 객체를 반환할 수 없으면 항상 에러를 일으켜야 합니다. 그렇지 않으면, 언피클러는
+            # None이 지속성 ID에 의해 참조되는 객체라고 생각할 것입니다.
             raise pickle.UnpicklingError("unsupported persistent object")
 
 
@@ -50,7 +47,7 @@ def main():
     import io
     import pprint
 
-    # Initialize and populate our database.
+    # 데이터베이스를 초기화하고 값을 채웁니다.
     conn = sqlite3.connect(":memory:")
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE memos(key INTEGER PRIMARY KEY, task TEXT)")
@@ -62,20 +59,20 @@ def main():
     for task in tasks:
         cursor.execute("INSERT INTO memos VALUES(NULL, ?)", (task,))
 
-    # Fetch the records to be pickled.
+    # 피클 할 레코드를 가져옵니다.
     cursor.execute("SELECT * FROM memos")
     memos = [MemoRecord(key, task) for key, task in cursor]
-    # Save the records using our custom DBPickler.
+    # 사용자 정의 DBPickler를 사용하여 레코드를 저장합니다.
     file = io.BytesIO()
     DBPickler(file).dump(memos)
 
     print("Pickled records:")
     pprint.pprint(memos)
 
-    # Update a record, just for good measure.
+    # 확인을 위해 레코드를 갱신합니다.
     cursor.execute("UPDATE memos SET task='learn italian' WHERE key=1")
 
-    # Load the records from the pickle data stream.
+    # 피클 데이터 스트림에서 레코드를 로드합니다.
     file.seek(0)
     memos = DBUnpickler(file, conn).load()
 
